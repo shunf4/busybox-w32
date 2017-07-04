53cc50514c1ffa3371d798bdd08a326f9f5437fe
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-07-04 23:09:38 +0200

    ash: when updating the pwd, give mingw_pathconv() a chance
    The newly-introduced mingw_pathconv() function attempts to replicate
    MSYS2's auto-conversion from /usr/bin to <MSYS2_ROOT>\usr\bin (where
    MSYS2_ROOT is relative to the executable, determined on-the-fly) and
    from /c/WINDOWS to c:\WINDOWS.
    
    It won't work, though, if we prefix those paths with the drive already,
    though. So let's not.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- shell/ash.c
+++ shell/ash.c
@@ -2663,7 +2663,7 @@ updatepwd(const char *dir)
 
 	cdcomppath = sstrdup(dir);
 	STARTSTACKSTR(new);
-	if (!absdrive && curdir == nullstr)
+	if (!absdrive && (curdir == nullstr || *dir == '/'))
 		return 0;
 	if (!abspath) {
 		if (curdir == nullstr)
