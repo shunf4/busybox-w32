shunf4/busybox-w32

commit 7af304a473d3dc20da215dc64215b656088eb419

win32: appletlib: special way to recognize applet name on Windows
argv[0] on Windows is always the executable path. We can't rely on it
to extract the applet name. For single busybox executable, just pass
an environment variable, BUSYBOX_APPLET_NAME.

Also strip .exe suffix when looking for applet name.

libbb/appletlib.c
@@ -806,6 +806,18 @@ int main(int argc UNUSED_PARAM, char **argv)
 	applet_name = argv[0];
 	if (applet_name[0] == '-')
 		applet_name++;
+	if (ENABLE_PLATFORM_MINGW32) {
+		const char *applet_name_env = getenv("BUSYBOX_APPLET_NAME");
+		if (applet_name_env && *applet_name_env) {
+			applet_name = applet_name_env;
+			unsetenv("BUSYBOX_APPLET_NAME");
+		}
+		else {
+			int len = strlen(applet_name);
+			if (len > 4 && !strcmp(applet_name+len-4, ".exe"))
+				argv[0][applet_name-argv[0]+len-4] = '\0';
+		}
+	}
 	applet_name = bb_basename(applet_name);
 
 	parse_config_file(); /* ...maybe, if FEATURE_SUID_CONFIG */

commit e4879def91a052f7a1e598be5f620fbba2a66ccf

Fix potential use-after-free
The getenv() function is inherently thread-unsafe: the returned pointers
are potentially invalidated whenever setenv() or unsetenv() is called.

This is not just some theoretical consideration: immediately after
obtaining the pointer to the BUSYBOX_APPLET_NAME variable's value, we
call unsetenv() for the very same environment variable.

Oddly enough, this invalidates the pointer only occasionally, and then
usually by resetting the value to the empty string. The symptom is a
seemingly random:

	: applet not found

Let's just strdup() the string; We do not even need to bother with
free()ing it because we allocate this in the main() function and the
process will simply exit after using the value in run_applet_and_exit(),
anyway.

This fixes a bug introduced in 7af304a47 (win32: appletlib: special way
to recognize applet name on Windows, 2010-04-05).

That commit also seems to purport that argv[0] *must* be the executable
path on Windows when spawning new processes. That is incorrect, however,
possibly making the entire BUSYBOX_APPLET_NAME workaround moot. But
that's a story for another day.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

libbb/appletlib.c
@@ -1079,7 +1079,7 @@ int main(int argc UNUSED_PARAM, char **argv)
 	if (ENABLE_PLATFORM_MINGW32) {
 		const char *applet_name_env = getenv("BUSYBOX_APPLET_NAME");
 		if (applet_name_env && *applet_name_env) {
-			applet_name = applet_name_env;
+			applet_name = xstrdup(applet_name_env);
 			unsetenv("BUSYBOX_APPLET_NAME");
 		}
 		else if ( argv[1] && argv[2] && strcmp(argv[1], "--busybox") == 0 ) {