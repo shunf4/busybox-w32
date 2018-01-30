a9edbf413742977dbd4b9f3146756042d6afa894
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-01-30 18:25:02 +0100

    Work around GCC warnings
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- editors/awk.c
+++ editors/awk.c
@@ -2100,7 +2100,7 @@ static char *awk_printf(node *n)
 	char *b = NULL;
 	char *fmt, *s, *f;
 	const char *s1;
-	int i, j, incr, bsize;
+	int i, j, incr, bsize = 0;
 	char c, c1;
 	var *v, *arg;
 
@@ -2163,7 +2163,7 @@ static int awk_sub(node *rn, const char *repl, int nm, var *src, var *dest, int 
 {
 	char *resbuf;
 	const char *sp;
-	int match_no, residx, replen, resbufsize;
+	int match_no, residx, replen, resbufsize = 0;
 	int regexec_flags;
 	regmatch_t pmatch[10];
 	regex_t sreg, *regex;
